e332801cc9e817360a154322708519bd8f1ca9cb
Kylmakalle <iceman9831@gmail.com>
2024-07-04 02:45:56 +0300

    Another attempt to fix badge on some Zoomed devices

--- submodules/Display/Source/DeviceMetrics.swift
+++ submodules/Display/Source/DeviceMetrics.swift
@@ -373,3 +373,18 @@ public enum DeviceMetrics: CaseIterable, Equatable {
         return self.hasTopNotch || self.hasDynamicIsland
     }
 }
+
+// MARK: Swifgram
+public extension DeviceMetrics {
+    
+    var deviceModelCode: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let modelCode = withUnsafePointer(to: &systemInfo.machine) {
+            $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+                ptr in String.init(validatingUTF8: ptr)
+            }
+        }
+        return modelCode ?? "unknown"
+    }
+}
