7334c8a87b49a7419fd9d8549974a760149ac566
levochkaa <levochkaaa@icloud.com>
2024-07-12 12:25:32 +0300

    change from subnodes to isHidden

--- submodules/TelegramUI/Sources/ChatControllerNode.swift
+++ submodules/TelegramUI/Sources/ChatControllerNode.swift
@@ -1457,19 +1457,20 @@ class ChatControllerNode: ASDisplayNode, ASScrollViewDelegate {
         var dismissedOverlayContextPanelNode: ChatInputContextPanelNode?
         // MARK: Swiftgram
         var inputPanelNodes = inputPanelForChatPresentationIntefaceState(self.chatPresentationInterfaceState, context: self.context, currentPanel: self.inputPanelNode, currentSecondaryPanel: self.secondaryInputPanelNode, textInputPanelNode: self.textInputPanelNode, interfaceInteraction: self.interfaceInteraction)
-        
-        if self.chatPresentationInterfaceState.interfaceState.selectionState != nil || self.chatPresentationInterfaceState.search != nil {
-            self.inputPanelClippingNode.addSubnode(self.inputPanelBackgroundNode)
-            self.inputPanelClippingNode.addSubnode(self.inputPanelBackgroundSeparatorNode)
-            self.inputPanelBackgroundNode.addSubnode(self.inputPanelBottomBackgroundSeparatorNode)
-        } else if (inputPanelNodes.primary != nil || inputPanelNodes.secondary != nil) && SGSimpleSettings.shared.hideChannelBottomButton {
-            // So there should be some panel, but user don't want it. Let's check if our logic will hide it
-            inputPanelNodes = inputPanelForChatPresentationIntefaceState(self.chatPresentationInterfaceState, context: self.context, currentPanel: self.inputPanelNode, currentSecondaryPanel: self.secondaryInputPanelNode, textInputPanelNode: self.textInputPanelNode, interfaceInteraction: self.interfaceInteraction, forceHideChannelButton: true)
-            if inputPanelNodes.primary == nil && inputPanelNodes.secondary == nil {
-                // Looks like we're eligible to hide the panel, let's remove safe area fill as well
-                self.inputPanelBackgroundNode.removeFromSupernode()
-                self.inputPanelBackgroundSeparatorNode.removeFromSupernode()
-                self.inputPanelBottomBackgroundSeparatorNode.removeFromSupernode()
+        if SGSimpleSettings.shared.hideChannelBottomButton {
+            if self.chatPresentationInterfaceState.interfaceState.selectionState != nil || self.chatPresentationInterfaceState.search != nil {
+                self.inputPanelBackgroundNode.isHidden = false
+                self.inputPanelBackgroundSeparatorNode.isHidden = false
+                self.inputPanelBottomBackgroundSeparatorNode.isHidden = false
+            } else if (inputPanelNodes.primary != nil || inputPanelNodes.secondary != nil)  {
+                // So there should be some panel, but user don't want it. Let's check if our logic will hide it
+                inputPanelNodes = inputPanelForChatPresentationIntefaceState(self.chatPresentationInterfaceState, context: self.context, currentPanel: self.inputPanelNode, currentSecondaryPanel: self.secondaryInputPanelNode, textInputPanelNode: self.textInputPanelNode, interfaceInteraction: self.interfaceInteraction, forceHideChannelButton: true)
+                if inputPanelNodes.primary == nil && inputPanelNodes.secondary == nil {
+                    // Looks like we're eligible to hide the panel, let's remove safe area fill as well
+                    self.inputPanelBackgroundNode.isHidden = true
+                    self.inputPanelBackgroundSeparatorNode.isHidden = true
+                    self.inputPanelBottomBackgroundSeparatorNode.isHidden = true
+                }
             }
         }
         
