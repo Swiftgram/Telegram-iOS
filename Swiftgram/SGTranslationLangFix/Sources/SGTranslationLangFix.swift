14e1a1e4e3480fbdd2db639465e9ff895b745ef6
Kylmakalle <iceman9831@gmail.com>
2024-12-01 14:30:45 +0200

    Fix German language variants

--- Swiftgram/SGTranslationLangFix/Sources/SGTranslationLangFix.swift
+++ Swiftgram/SGTranslationLangFix/Sources/SGTranslationLangFix.swift
@@ -1,5 +1,7 @@
 public func sgTranslationLangFix(_ language: String) -> String {
-    if language.hasPrefix("zh-") {
+    if language.hasPrefix("de-") {
+        return "de"
+    } else if language.hasPrefix("zh-") {
         return "zh"
     } else {
         return language
