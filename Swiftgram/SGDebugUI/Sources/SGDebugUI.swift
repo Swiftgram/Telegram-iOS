f5ff86c82e2715446ff529793d7a0d305b541bcd
Kylmakalle <iceman9831@gmail.com>
2024-12-26 20:12:54 +0200

    WIP import/backup

--- Swiftgram/SGDebugUI/Sources/SGDebugUI.swift
+++ Swiftgram/SGDebugUI/Sources/SGDebugUI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UniformTypeIdentifiers
 import SGItemListUI
 import UndoUI
 import AccountContext
@@ -23,7 +24,10 @@ private enum SGDebugControllerSection: Int32, SGItemListSection {
 
 private enum SGDebugActions: String {
     case flexing
+    case fileManager
     case clearRegDateCache
+    case accountsBackup
+    case accountsImport
 }
 
 private enum SGDebugToggles: String {
@@ -40,6 +44,10 @@ private func SGDebugControllerEntries(presentationData: PresentationData) -> [SG
     let id = SGItemListCounter()
     #if DEBUG
     entries.append(.action(id: id.count, section: .base, actionType: .flexing, text: "FLEX", kind: .generic))
+    entries.append(.action(id: id.count, section: .base, actionType: .fileManager, text: "FileManager", kind: .generic))
+    
+    entries.append(.action(id: id.count, section: .base, actionType: .accountsBackup, text: "Backup", kind: .generic))
+    entries.append(.action(id: id.count, section: .base, actionType: .accountsImport, text: "Import", kind: .generic))
     #endif
     entries.append(.action(id: id.count, section: .base, actionType: .clearRegDateCache, text: "Clear Regdate cache", kind: .generic))
     entries.append(.toggle(id: id.count, section: .base, settingName: .forceImmediateShareSheet, value: SGSimpleSettings.shared.forceSystemSharing, text: "Force System Share Sheet", enabled: true))
@@ -88,6 +96,76 @@ public func sgDebugController(context: AccountContext) -> ViewController {
             #if DEBUG
             FLEXManager.shared.toggleExplorer()
             #endif
+        case .fileManager:
+            #if DEBUG
+            let baseAppBundleId = Bundle.main.bundleIdentifier!
+            let appGroupName = "group.\(baseAppBundleId)"
+            let maybeAppGroupUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupName)
+            if let maybeAppGroupUrl = maybeAppGroupUrl {
+                if let fileManager = FLEXFileBrowserController(path: maybeAppGroupUrl.path) {
+                    FLEXManager.shared.showExplorer()
+                    let flexNavigation = FLEXNavigationController(rootViewController: fileManager)
+                    FLEXManager.shared.presentTool({ return flexNavigation })
+                }
+            } else {
+                presentControllerImpl?(UndoOverlayController(
+                    presentationData: presentationData,
+                    content: .info(title: nil, text: "Empty path",timeout: nil, customUndoText: nil),
+                    elevatedLayout: false,
+                    action: { _ in return false }
+                ),
+                nil)
+            }
+            #endif
+        case .accountsBackup:
+            #if DEBUG
+            
+            let signal = context.sharedContext.accountManager.accountRecords()
+            |> take(1)
+            |> deliverOnMainQueue
+            let _ = signal.start(next: { view in
+                var recordsToBackup: [Int64: AccountRecord<TelegramAccountManagerTypes.Attribute>] = [:]
+                for record in view.records {
+                    var sortOrder: Int32 = 0
+                    var isLoggedOut: Bool = false
+                    var isTestingEnvironment: Bool = false
+                    var userId: Int64 = 0
+                    for attribute in record.attributes {
+                        if case let .sortOrder(value) = attribute {
+                            sortOrder = value.order
+                        } else if case .loggedOut = attribute  {
+                            isLoggedOut = true
+                        } else if case let .environment(environment) = attribute, case .test = environment.environment {
+                            isTestingEnvironment = true
+                        } else if case let .backupData(backupData) = attribute {
+                            userId = backupData.data?.peerId ?? 0
+                        }
+                    }
+                    let _ = sortOrder
+                    let _ = isTestingEnvironment
+                    
+                    if !isLoggedOut {
+                        recordsToBackup[userId] = record
+                    }
+                }
+                
+                do {
+                    let jsonData = try JSONEncoder().encode(recordsToBackup)
+                    let maybeJsonString = String(data: jsonData, encoding: .utf8)
+                    guard let jsonString = maybeJsonString else {
+                        throw NSError(domain: "JSONProcessing", code: 1, userInfo: [NSLocalizedDescriptionKey: "JSON string is nil"])
+                    }
+                    print("EXPORTED", jsonString)
+                } catch let e {
+                    print("EXPORT ERROR: \(e)")
+                }
+                
+            })
+            
+            #endif
+            
+        case .accountsImport:
+            preconditionFailure()
         }
     })
     
