2ddba21088aad0fe4f5eb2f533a3938cd1b7a0c3
Kylmakalle <iceman9831@gmail.com>
2025-07-10 01:30:38 +0300

    Disable voice/video recording tooltip, closes #121

--- submodules/TelegramUI/Sources/ChatController.swift
+++ submodules/TelegramUI/Sources/ChatController.swift
@@ -9136,6 +9136,7 @@ public final class ChatControllerImpl: TelegramBaseController, ChatController, G
     }
     
     func displayMediaRecordingTooltip() {
+        if ({ return true })() { return } // MARK: Swiftgram
         guard let peer = self.presentationInterfaceState.renderedPeer?.peer else {
             return
         }
