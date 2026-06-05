111292eba07caa557ca3d6eb5e0e5bc9667b063d
Vladislav <lawliet2012sven@gmail.com>
2026-06-05 16:44:47 +0500

    MtProtoKit: align FakeTLS ClientHello with current Chrome (JA3/JA4) + per-connection extension shuffle

--- submodules/MtProtoKit/Sources/MTTcpConnection.m
+++ submodules/MtProtoKit/Sources/MTTcpConnection.m
@@ -164,7 +164,9 @@ typedef enum {
     HelloGenerationCommandBeginChoice = 10,
     HelloGenerationCommandBeginAlternative = 11,
     HelloGenerationCommandEndAlternative = 12,
-    HelloGenerationCommandEndChoice = 13
+    HelloGenerationCommandEndChoice = 13,
+    HelloGenerationCommandBeginShuffle = 14,
+    HelloGenerationCommandEndShuffle = 15
 } HelloGenerationCommand;
 
 typedef struct {
@@ -203,6 +205,10 @@ static HelloGenerationCommand parseCommand(NSString *string, HelloParseState *st
             return HelloGenerationCommandEndAlternative;
         } else if (c == '>') {
             return HelloGenerationCommandEndChoice;
+        } else if (c == '{') {
+            return HelloGenerationCommandBeginShuffle;
+        } else if (c == '}') {
+            return HelloGenerationCommandEndShuffle;
         } else {
             return HelloGenerationCommandInvalid;
         }
@@ -459,11 +465,42 @@ static bool executeGenerationCodeRecursive(NSString *code, HelloParseState *stat
                 [resultData appendData:alternatives[choiceIndex]];
                 break;
             }
+            case HelloGenerationCommandBeginShuffle: {
+                NSMutableArray<NSMutableData *> *items = [[NSMutableArray alloc] init];
+                while (true) {
+                    HelloGenerationCommand nextCommand = parseCommand(code, state);
+                    if (nextCommand == HelloGenerationCommandEndShuffle) {
+                        break;
+                    }
+                    if (nextCommand != HelloGenerationCommandBeginAlternative) {
+                        return false;
+                    }
+                    NSMutableData *itemData = [[NSMutableData alloc] init];
+                    NSMutableArray<NSNumber *> *itemLengthStack = [[NSMutableArray alloc] init];
+                    if (!executeGenerationCodeRecursive(code, state, grease, domain, provider, itemData, itemLengthStack)) {
+                        return false;
+                    }
+                    if (itemLengthStack.count != 0) {
+                        return false;
+                    }
+                    [items addObject:itemData];
+                }
+                for (NSUInteger k = items.count; k > 1;) {
+                    k -= 1;
+                    uint32_t r = arc4random_uniform((uint32_t)(k + 1));
+                    [items exchangeObjectAtIndex:k withObjectAtIndex:r];
+                }
+                for (NSMutableData *itemData in items) {
+                    [resultData appendData:itemData];
+                }
+                break;
+            }
             case HelloGenerationCommandEndAlternative: {
                 // signals end of current alternative — return to caller
                 return true;
             }
             case HelloGenerationCommandBeginAlternative:
+            case HelloGenerationCommandEndShuffle:
             case HelloGenerationCommandEndChoice: {
                 // should not be encountered directly at this level
                 return false;
@@ -478,52 +515,78 @@ static bool executeGenerationCodeRecursive(NSString *code, HelloParseState *stat
 }
 
 static NSMutableData *executeGenerationCode(id<EncryptionProvider> provider, NSData *domain) {
-    NSString *code = @"S \"\\x16\\x03\\x01\"\n"
-                      "[\n"
-                      "S \"\\x01\\x00\"\n"
-                      "[\n"
-                      "S \"\\x03\\x03\"\n"
-                      "Z 32\n"
-                      "S \"\\x20\"\n"
-                      "R 32\n"
-                      "<\n"
-                      "( S \"\\x00\\x1c\" G 0 S \"\\x13\\x02\\x13\\x01\\x13\\x03\\xc0\\x2c\\xc0\\x30\\xc0\\x2b\\xcc\\xa9\\xc0\\x2f\\xcc\\xa8\\xc0\\x0a\\xc0\\x09\\xc0\\x14\\xc0\\x13\" )\n"
-                      "( S \"\\x00\\x2a\" G 0 S \"\\x13\\x02\\x13\\x03\\x13\\x01\\xc0\\x2c\\xc0\\x2b\\xcc\\xa9\\xc0\\x30\\xc0\\x2f\\xcc\\xa8\\xc0\\x0a\\xc0\\x09\\xc0\\x14\\xc0\\x13\\x00\\x9d\\x00\\x9c\\x00\\x35\\x00\\x2f\\xc0\\x08\\xc0\\x12\\x00\\x0a\" )\n"
-                      ">\n"
-                      "S \"\\x01\\x00\"\n"
-                      "[\n"
-                      "G 2\n"
-                      "S \"\\x00\\x00\\x00\\x00\"\n"
-                      "[\n"
-                      "[\n"
-                      "S \"\\x00\"\n"
-                      "[\n"
-                      "D\n"
-                      "]\n"
-                      "]\n"
-                      "]\n"
-                      "S \"\\x00\\x17\\x00\\x00\\xff\\x01\\x00\\x01\\x00\\x00\\x0a\\x00\\x0e\\x00\\x0c\"\n"
-                      "G 4\n"
-                      "S \"\\x11\\xec\\x00\\x1d\\x00\\x17\\x00\\x18\\x00\\x19\\x00\\x0b\\x00\\x02\\x01\\x00\"\n"
-                      "<\n"
-                      "( S \"\\x00\\x10\\x00\\x0b\\x00\\x09\\x08\\x68\\x74\\x74\\x70\\x2f\\x31\\x2e\\x31\" )\n"
-                      "( S \"\\x00\\x10\\x00\\x0e\\x00\\x0c\\x02\\x68\\x32\\x08\\x68\\x74\\x74\\x70\\x2f\\x31\\x2e\\x31\" )\n"
-                      ">\n"
-                      "S \"\\x00\\x05\\x00\\x05\\x01\\x00\\x00\\x00\\x00\\x00\\x0d\\x00\\x16\\x00\\x14\\x04\\x03\\x08\\x04\\x04\\x01\\x05\\x03\\x08\\x05\\x08\\x05\\x05\\x01\\x08\\x06\\x06\\x01\\x02\\x01\\x00\\x12\\x00\\x00\\x00\\x33\\x04\\xef\\x04\\xed\"\n"
-                      "G 4\n"
-                      "S \"\\x00\\x01\\x00\\x11\\xec\\x04\\xc0\"\n"
-                      "M\n"
-                      "K\n"
-                      "S \"\\x00\\x1d\\x00\\x20\"\n"
-                      "K\n"
-                      "S \"\\x00\\x2d\\x00\\x02\\x01\\x01\\x00\\x2b\\x00\\x07\\x06\"\n"
-                      "G 6\n"
-                      "S \"\\x03\\x04\\x03\\x03\\x00\\x1b\\x00\\x03\\x02\\x00\\x01\"\n"
-                      "G 3\n"
-                      "S \"\\x00\\x01\\x00\"\n"
-                      "]\n"
-                      "]\n"
-                      "]\n";
+    NSString *code = @"S \"\x16\x03\x01\"
+"
+                      "[
+"
+                      "S \"\x01\x00\"
+"
+                      "[
+"
+                      "S \"\x03\x03\"
+"
+                      "Z 32
+"
+                      "S \"\x20\"
+"
+                      "R 32
+"
+                      "S \"\x00\x20\"
+"
+                      "G 0
+"
+                      "S \"\x13\x01\x13\x02\x13\x03\xc0\x2b\xc0\x2f\xc0\x2c\xc0\x30\xcc\xa9\xcc\xa8\xc0\x13\xc0\x14\x00\x9c\x00\x9d\x00\x2f\x00\x35\"
+"
+                      "S \"\x01\x00\"
+"
+                      "[
+"
+                      "{
+"
+                      "( G 2 S \"\x00\x00\" )
+"
+                      "( S \"\x00\x0a\x00\x0c\x00\x0a\" G 4 S \"\x11\xec\x00\x1d\x00\x17\x00\x18\" )
+"
+                      "( S \"\x00\x0b\x00\x02\x01\x00\" )
+"
+                      "( S \"\x00\x00\" [ [ S \"\x00\" [ D ] ] ] )
+"
+                      "( S \"\x00\x23\x00\x00\" )
+"
+                      "( S \"\x00\x17\x00\x00\" )
+"
+                      "( S \"\xff\x01\x00\x01\x00\" )
+"
+                      "( S \"\x00\x05\x00\x05\x01\x00\x00\x00\x00\" )
+"
+                      "( S \"\x00\x0d\x00\x12\x00\x10\x04\x03\x08\x04\x04\x01\x05\x03\x08\x05\x05\x01\x08\x06\x06\x01\" )
+"
+                      "( S \"\x00\x12\x00\x00\" )
+"
+                      "( S \"\x00\x10\x00\x0e\x00\x0c\x02\x68\x32\x08\x68\x74\x74\x70\x2f\x31\x2e\x31\" )
+"
+                      "( S \"\x00\x33\x04\xef\x04\xed\" G 4 S \"\x00\x01\x00\x11\xec\x04\xc0\" M K S \"\x00\x1d\x00\x20\" K )
+"
+                      "( S \"\x00\x2d\x00\x02\x01\x01\" )
+"
+                      "( S \"\x00\x2b\x00\x07\x06\" G 6 S \"\x03\x04\x03\x03\" )
+"
+                      "( S \"\xfe\x0d\x00\x10\" Z 16 )
+"
+                      "( S \"\x44\xcd\x00\x05\x00\x03\x02\x68\x32\" )
+"
+                      "( S \"\x00\x1b\x00\x03\x02\x00\x02\" )
+"
+                      "( G 3 S \"\x00\x00\" )
+"
+                      "}
+"
+                      "]
+"
+                      "]
+"
+                      "]
+";
 
     uint8_t grease[8];
     if (!MTGenerateGreaseValues(grease)) {
